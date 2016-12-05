89ef61dcd6c9aabd0b57ea56d62e467c06302621
mizworski <mi360968@students.mimuw.edu.pl>
2016-12-05 21:35:07 +0100

    Initial commit

---
+++ src/gra/MojaPlansza.java
@@ -0,0 +1,28 @@
+package gra;
+
+public class MojaPlansza implements Plansza{
+    
+  public MojaPlansza(int wysokość, int szerokość) {
+
+  }
+
+  @Override
+  public void postaw(Postać postać, int wiersz, int kolumna) throws InterruptedException {
+
+  }
+
+  @Override
+  public void przesuń(Postać postać, Kierunek kierunek) throws InterruptedException, DeadlockException {
+
+  }
+
+  @Override
+  public void usuń(Postać postać) {
+
+  }
+
+  @Override
+  public void sprawdź(int wiersz, int kolumna, Akcja jeśliZajęte, Runnable jeśliWolne) {
+
+  }
+}
